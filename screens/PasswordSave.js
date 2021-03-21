86f0b8085f850105ba72860738261af8c55b077c
UditSamanta <68061568+UditSamanta@users.noreply.github.com>
2021-03-21 16:57:19 +0530

    Add files via upload

---
+++ screens/PasswordSave.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class PasswordSave extends React.Component{
+    render() {
+        return(
+            <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
+        <Text>Password Save</Text>
+            </View>
+        )
+    }
+}
